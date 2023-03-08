93f744e4537fb94370524fbbec76c3f59755dc80
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-08 10:46:32 +0100

    update

--- raspberry_setup/setup_basic/setup-basic.sh
+++ raspberry_setup/setup_basic/setup-basic.sh
@@ -6,5 +6,10 @@ sudo apt-get update && sudo apt-get upgrade -y
 # install relevant packages
 sudo apt install libpcsclite-dev pcscd pcsc-tools libssl-dev libengine-pkcs11-openssl autoconf libtool openssl git python3-venv -y
 
+# Upgrade pip
+sudo python3 -m pip install --upgrade pip
+
 # Clone the devid_api repository
 git clone https://github.com/FHKatCSW/devid_api.git
+git clone https://github.com/FHKatCSW/devid_raspi.git
+git clone https://github.com/FHKatCSW/devid_nameplate.git
--- raspberry_setup/setup_gui/setup-gui.sh
+++ raspberry_setup/setup_gui/setup-gui.sh
@@ -3,15 +3,19 @@
 # Define the path to the virtual environment
 VENV_PATH="/home/admin/devid_nameplate/nameplate_env"
 
+# Set the working directory for the PyQt application
+APP_WORKDIR="/home/admin/devid_nameplate"
 
 # create a new virtual environment
 python -m venv $VENV_PATH
 
 # Activate the virtual environment
 source "$VENV_PATH/bin/activate"
+# source "/home/admin/devid_nameplate/nameplate_env/bin/activate"
 
 # Install the requirements
-pip install -r requirements.txt
+pip install -r "$APP_WORKDIR/requirements.txt"
+# pip install -r "/home/admin/devid_nameplate/requirements.txt"
 
 # Freeze the requirements
 pip freeze > requirements.txt
@@ -25,8 +29,7 @@ APP_PATH="/home/admin/devid_nameplate/run.py"
 # Define the path to the systemd service file
 SERVICE_FILE="/etc/systemd/system/pyqt_application.service"
 
-# Set the working directory for the PyQt application
-APP_WORKDIR="/home/admin/devid_nameplate"
+
 
 echo "[Unit]
 Description=IEEE 802.1 AR GUI
@@ -49,4 +52,11 @@ sudo systemctl start pyqt_application.service
 # Enable the PyQT application service to start on boot
 sudo systemctl enable pyqt_application.service
 
+
+# -------------------------
+# Additional commands
+# -------------------------
+
+# Show the last 100 logs
 # journalctl --unit=pyqt_application.service -n 100 --no-pager
+
