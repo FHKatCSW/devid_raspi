f97464f7b596118f5f4e77d43617332a27079a43
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-09 11:21:48 +0100

    update

--- setup_files/06_setup_rest_api/setup-rest-api.sh
+++ setup_files/06_setup_rest_api/setup-rest-api.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
+# Set the working directory for the PyQt application
+APP_WORKDIR="/home/$SUDO_USER/devid_api"
+
 # Define the path to the virtual environment
 VENV_PATH="/home/$SUDO_USER/devid_api/env"
 
-# Activate the virtual environment
-source "$VENV_PATH/bin/activate"
+# Define the path to the systemd service file
+SERVICE_FILE="/etc/systemd/system/devid-api.service"
 
 # Define the path to the Flask app within the virtual environment
 APP_PATH="/home/$SUDO_USER/devid_api/run.py"
-
-# Define the name of the Flask app module
-APP_MODULE="app"
 
 # Define the IP address and port for the Flask app to run on
 IP_ADDRESS="0.0.0.0"
 PORT="5000"
 
-# Define the path to the systemd service file
-SERVICE_FILE="/etc/systemd/system/devid-api.service"
+python -m venv "$VENV_PATH"
+
+# Activate the virtual environment
+source "$VENV_PATH/bin/activate"
+
+# Install the requirements
+pip install -r "$APP_WORKDIR/requirements.txt"
 
 # Write the systemd service file
 echo "[Unit]
-Description=Flask API
+Description=IEEE 802.1 AR REST API
+After=network.target
 
 [Service]
 User=$SUDO_USER
+Environment="FLASK_APP=run.py"
 WorkingDirectory=$APP_PATH
-Environment=\"PATH=$VENV_PATH/bin\"
-ExecStart=$VENV_PATH/bin/gunicorn $APP_MODULE:app -b $IP_ADDRESS:$PORT --log-file /var/log/devid-api.log
+ExecStart=$VENV_PATH/bin/flask run --host=$IP_ADDRESS --port=$PORT
 Restart=always
 
 [Install]
@@ -41,3 +47,13 @@ sudo systemctl start devid-api
 
 # Enable the devid API service to start automatically on boot
 sudo systemctl enable devid-api
+
+# -------------------------
+# Additional commands
+# -------------------------
+
+# Show the last 100 logs
+# journalctl --unit=devid-api.service -n 100 --no-pager
+
+# Restart the service
+# sudo systemctl restart devid-api.service
