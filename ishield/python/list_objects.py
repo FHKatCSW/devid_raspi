6e6c79bff94c8b8046efb792a94c0028d4b43ddf
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-10 11:52:17 +0100

    update

--- ishield/python/list_objects.py
+++ ishield/python/list_objects.py
@@ -1,22 +1,25 @@
-from pkcs11 import PKCS11Lib, ObjectClass
+import json
+from pyhsm.hsmclient import HsmClient
 
+def list_objects_on_hsm(slot_num, pin, library_path):
+    result = []
+    with HsmClient(slot=slot_num, pin=pin, pkcs11_lib=library_path) as c:
+        obj_list = c.get_objects()
+        for obj in obj_list:
+            object_info = {
+                "label": obj.label,
+                "class": obj.object_class,
+                "id": obj.id,
+                "attributes": obj.get_attributes()
+            }
+            result.append(object_info)
+    return json.dumps(result)
 
-def list_objects_on_hsm(library_path, token_label, user_pin):
-    pkcs11 = PKCS11Lib().load(library_path)
-    slot = pkcs11.get_token_slots(token_label=token_label)[0]
-    session = slot.open(user_pin=user_pin)
-
-    objects = session.get_objects({
-        ObjectClass: ObjectClass.DATA,
-    })
-
-    for obj in objects:
-        print(f'Label: {obj.label}, Class: {obj.object_class}')
 
 
 if __name__ == "__main__":
     list_objects_on_hsm(
         library_path='/usr/lib/opensc-pkcs11.so',
-        token_label='MyToken',
-        user_pin='1234'
+        slot_num='0',
+        pin='1234'
     )
