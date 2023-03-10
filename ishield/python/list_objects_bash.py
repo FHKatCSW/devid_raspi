71dec4d1f520e3952252e50a7f0a4f2525e470a4
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-10 14:24:21 +0100

    update

--- ishield/python/list_objects_bash.py
+++ ishield/python/list_objects_bash.py
@@ -15,8 +15,8 @@ class HsmObjects:
     def list_objects_on_hsm(self):
         # Run the bash script and capture the output
         result = subprocess.check_output(["./bash/list_objects.sh", self.library_path, str(self.slot_num), self.pin])
-
-        return result
+        result_byte = str.encode(result)
+        return result_byte
 
     def parse_input_str(self, input_str):
         pattern = r'^\s*([^:]+):\s*(.*?)\s*$'
