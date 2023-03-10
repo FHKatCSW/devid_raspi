e77e23ecf950a3a6642507c104e75a9990ca636c
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-10 11:32:04 +0100

    update

--- ishield/python/list_slots.py
+++ ishield/python/list_slots.py
@@ -1,12 +1,11 @@
-import pkcs11
-
+from pyhsm.hsmclient import HsmClient
 
 def list_available_slots(library_path):
-    pkcs11 = pkcs11.lib(library_path)
-    slots = pkcs11.get_slots()
 
-    for slot in slots:
-        print(f'Slot {slot.slot_number}: {slot.slot_info.manufacturerID} {slot.slot_info.description}')
+    with HsmClient(pkcs11_lib=library_path) as c:
+        for s in c.get_slot_info():
+            print("----------------------------------------")
+            print(s.to_string())
 
 if __name__ == "__main__":
     list_available_slots(library_path='/usr/lib/opensc-pkcs11.so')
