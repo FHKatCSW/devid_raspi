c69aad11a30f83943dd636837aaffb7ad8de4f5b
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-10 12:43:59 +0100

    update

--- ishield/python/list_objects_bash.py
+++ ishield/python/list_objects_bash.py
@@ -6,10 +6,10 @@ def list_objects_on_hsm(library_path, slot_num, pin):
     result = subprocess.check_output(["./bash/list_objects.sh", library_path, str(slot_num), pin])
 
     # Decode the JSON-encoded object list
-    #object_list = json.loads(result)
+    object_list = json.loads(result)
 
     # Return the object list as a Python list of dictionaries
-    print(result)
+    print(object_list)
 
 if __name__ == "__main__":
     list_objects_on_hsm(
