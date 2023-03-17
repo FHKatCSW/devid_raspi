87b4f0571fc99fa34f433efecd6756e0ba4eb895
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-17 15:36:15 +0100

    update

--- setup_raspberry.sh
+++ setup_raspberry.sh
@@ -26,26 +26,26 @@ if [[ ! -d "/home/$SUDO_USER/devid_raspi" ]]; then
 fi
 
 header Basic setup
-sudo bash /home/"$SUDO_USER"/devid_raspi/setup_files/01_setup_basic/setup-basic.sh
+bash /home/"$SUDO_USER"/devid_raspi/setup_files/01_setup_basic/setup-basic.sh
 
 header Setup the iShield
-sudo bash /home/"$SUDO_USER"/devid_raspi/setup_files/02_setup_ishield/setup-ishield.sh
+bash /home/"$SUDO_USER"/devid_raspi/setup_files/02_setup_ishield/setup-ishield.sh
 
 header Setup py-hsm
-sudo bash /home/"$SUDO_USER"/devid_raspi/setup_files/03_setup_pyhsm/setup-pyhsm.sh
+bash /home/"$SUDO_USER"/devid_raspi/setup_files/03_setup_pyhsm/setup-pyhsm.sh
 
 header Setup HSM Log service
-sudo bash /home/"$SUDO_USER"/devid_raspi/setup_files/04_setup_hsm_log_service/setup-hsm-logger.sh
+bash /home/"$SUDO_USER"/devid_raspi/setup_files/04_setup_hsm_log_service/setup-hsm-logger.sh
 
 header Setup Certificate Storage
-sudo bash /home/"$SUDO_USER"/devid_raspi/setup_files/05_setup_certificate_storage/setup-certificate-storgae.sh
+bash /home/"$SUDO_USER"/devid_raspi/setup_files/05_setup_certificate_storage/setup-certificate-storgae.sh
 
 header Setup REST API
-sudo bash /home/"$SUDO_USER"/devid_raspi/setup_files/06_setup_rest_api/setup-rest-api.sh
+bash /home/"$SUDO_USER"/devid_raspi/setup_files/06_setup_rest_api/setup-rest-api.sh
 
 header Setup GUI
-sudo bash /home/"$SUDO_USER"/devid_raspi/setup_files/07_setup_gui/setup-gui.sh
+bash /home/"$SUDO_USER"/devid_raspi/setup_files/07_setup_gui/setup-gui.sh
 
 warn This is the last step: Raspberry will reboot afterwards
 header Setup Display
-sudo bash /home/"$SUDO_USER"/devid_raspi/setup_files/08_setup_display/setup-display.sh
+bash /home/"$SUDO_USER"/devid_raspi/setup_files/08_setup_display/setup-display.sh
