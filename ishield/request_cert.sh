fb9dd83b405910ba8fe40ab881ed17061e998823
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-20 12:19:50 +0100

    update

--- ishield/request_cert.sh
+++ ishield/request_cert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Usage
-# bash request_cert.sh "https://campuspki.germanywestcentral.cloudapp.azure.com" "foo123" "home/admin/raspi_auth_v1.p12" "home/admin/setup_test_3.csr"
+# bash request_cert.sh "https://campuspki.germanywestcentral.cloudapp.azure.com" "foo123" "/home/admin/raspi_auth_v1.p12" "/home/admin/setup_test_3.csr"
 
 EJBCA_BASE_URL=$1
 P12_TOKEN=$2
@@ -12,8 +12,8 @@ CSR_FILE=$4
 curl_response=$(curl -k -X POST \
 --header "Content-Type: application/json" \
 --header "Accept: application/json" \
---cert $P12_TOKEN:<$P12_PASS> \
---data '{"certificateRequest":"'$(cat $CSR_FILE)'"}' \
+--cert cat $P12_TOKEN | base64 | tr -d '\n':<$P12_PASS> \
+--data '{"certificateRequest":"'$(cat $CSR_FILE | tr -d '\n' | sed 's/-----END.*CERTIFICATE-----/-----END CERTIFICATE-----/g' | sed 's/-----BEGIN.*CERTIFICATE-----/-----BEGIN CERTIFICATE-----/g')'"}' \
 "$EJBCA_BASE_URL/ejbca/ejbca-rest-api/v1/certificate/generateCertificate")
 
 certificate=$(echo $curl_response | jq -r '.certificate')
