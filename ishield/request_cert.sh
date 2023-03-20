b9a6ac87601bc9e112c6f0aa489afdd86e776ea9
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-20 13:00:41 +0100

    update

--- ishield/request_cert.sh
+++ ishield/request_cert.sh
@@ -8,15 +8,29 @@ P12_TOKEN=$2
 P12_PASS=$3
 CSR_FILE=$4
 
+cert_profile_name="DeviceIdentity-Raspberry"
+ee_profile_name="KF-CS-EE-DeviceIdentity-Raspberry"
+ca_name="KF-CS-HMI-2023-CA"
+username="setup_test"
+enrollment_code="foo123"
 
-curl_response=$(curl -k -X POST \
---header "Content-Type: application/json" \
---header "Accept: application/json" \
---cert $P12_TOKEN:$P12_PASS \
---data '{"certificateRequest":"'$(cat $CSR_FILE | tr -d '\n' | sed 's/-----END.*CERTIFICATE-----/-----END CERTIFICATE-----/g' | sed 's/-----BEGIN.*CERTIFICATE-----/-----BEGIN CERTIFICATE-----/g')'"}' \
-"$EJBCA_BASE_URL/ejbca/ejbca-rest-api/v1/certificate/generateCertificate")
+csr=$(cat $CSR_FILE)
+template='{"certificate_request":$csr, "certificate_profile_name":$cp, "end_entity_profile_name":$eep, "certificate_authority_name":$ca, "username":$ee, "password":$pwd}'
+json_payload=$(jq -n \
+    --arg csr "$csr" \
+    --arg cp "$cert_profile_name" \
+    --arg eep "$ee_profile_name" \
+    --arg ca "$ca_name" \
+    --arg ee "$username" \
+    --arg pwd "$enrollment_code" \
+    "$template")
 
-echo $curl_response
-#certificate=$(echo $curl_response | jq -r '.certificate')
 
-#echo $certificate > certificate.pem
+curl -X POST -s \
+    --cert-type P12 \
+    --cert "$P12_TOKEN:$P12_PASS" \
+    -H 'Content-Type: application/json' \
+    --data "$json_payload" \
+    "https://$EJBCA_BASE_URL/ejbca/ejbca-rest-api/v1/certificate/pkcs10enroll" \
+    | jq .
+
