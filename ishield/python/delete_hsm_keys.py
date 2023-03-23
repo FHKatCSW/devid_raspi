d72ea9eee5b7a6a19c37d2a29635fef5698edb3d
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-23 13:12:19 +0100

    update

--- ishield/python/delete_hsm_keys.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import json
-
-class HsmCleaner:
-    def __init__(self, json_str):
-        self.keys = json.loads(json_str)
-
-
---
+++ ishield/python/test.py
@@ -0,0 +1,10 @@
+from hsm_objects import HsmObjects
+
+
+if __name__ == "__main__":
+    hsm_objects = HsmObjects(
+        slot_num=0,
+        pin='1234'
+    )
+
+    hsm_objects.delete_all_keys()
