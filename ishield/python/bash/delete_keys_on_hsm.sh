7ab3b3a24f39fa1da4c06f2204593621aa0eafa3
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-24 14:03:24 +0100

    update

--- ishield/python/bash/delete_keys_on_hsm.sh
+++ ishield/python/bash/delete_keys_on_hsm.sh
@@ -46,7 +46,7 @@ echo "label = $label"
 PKCS11_TOOL=/usr/bin/pkcs11-tool
 
 if [[ -n "$id" ]]; then
-  $PKCS11_TOOL --delete-object --type "$key_type"key --id "$id" --login --pin "$1"
+  $PKCS11_TOOL --delete-object --type "$key_type"key --id=$id --login --pin "$1"
 else
   $PKCS11_TOOL --delete-object --type "$key_type"key --label "$label" --login --pin "$1"
 fi
