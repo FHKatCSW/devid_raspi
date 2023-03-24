a959813c9f69c3e2254ee38fbd70e388f5a53e0d
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-03-24 14:05:13 +0100

    update

--- ishield/python/hsm_objects.py
+++ ishield/python/hsm_objects.py
@@ -89,7 +89,7 @@ class HsmObjects:
 
     def delete_key(self, priv_pub_key, key_id):
 
-        command = ['./bash/delete_keys_on_hsm.sh', priv_pub_key, f'-i "{key_id}"', self.pin]
+        command = ['./bash/delete_keys_on_hsm.sh', priv_pub_key, f'-i {key_id}', self.pin]
         print("Executing command:", " ".join(command))
         subprocess.call(command)
 
