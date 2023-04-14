93f3cd55c37a3a5a40876930671e998e901e7454
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-04-14 13:25:03 +0200

    update

--- setup_files/01_setup_basic/setup-basic.sh
+++ setup_files/01_setup_basic/setup-basic.sh
@@ -17,4 +17,25 @@ git clone https://github.com/FHKatCSW/devid_api.git
 git clone https://github.com/FHKatCSW/devid_raspi.git
 git clone https://github.com/FHKatCSW/devid_nameplate.git
 
+# Define the static IP address settings
+IP_ADDRESS="192.168.1.2/24"
+GATEWAY="192.168.1.1"
+DNS_SERVERS="192.168.1.3"
+
+# Backup the existing dhcpcd.conf file
+sudo cp /etc/dhcpcd.conf /etc/dhcpcd.conf.backup
+
+# Update the dhcpcd.conf file with static IP address settings
+sudo tee -a /etc/dhcpcd.conf > /dev/null << EOL
+
+# Added by script to configure static IP address
+interface eth0
+static ip_address=$IP_ADDRESS
+static routers=$GATEWAY
+static domain_name_servers=$DNS_SERVERS
+EOL
+
+echo "Static IP address has been configured in /etc/dhcpcd.conf!"
+
+
 echo "✅ basic setup finished"
